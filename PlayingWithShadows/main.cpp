a9d59ae635cadf3540f4bff71af649b70b972573
Filipe Marques <filipe.marques@mind.pt>
2020-02-12 10:42:15 +0000

    Initial Commit

---
+++ PlayingWithShadows/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PlayingWithShadows
+//
+//  Created by Filipe Marques on 12/02/2020.
+//  Copyright © 2020 Filipe Marques. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
